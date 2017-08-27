054cd641253753f21717be54c6af6041351f5f7b
Saiteja Reddy <saiteja0317@gmail.com>
2017-08-27 23:32:16 +0530

    Added Read Command Basic

--- Shell/main.c
+++ Shell/main.c
@@ -4,18 +4,25 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <string.h>
+#include <stdlib.h>
+
+void shell_loop(void);
+char* readCommand(void);
+
+char hostname[256];
+char username[256];
+char cwd[1024];
+int pos;
 
 int main(int argc, char const *argv[])
 {
-	char hostname[256];
 	int i,j,flag;
 	int userid = getuid();
 	struct passwd *uinfo = getpwuid(userid);
 	gethostname(hostname, 200);
-	char cwd[1024];
 	getwd(cwd);
-	char *username = uinfo->pw_name ;
-	int pos = 0;
+	strcpy(username,uinfo->pw_name) ;
+	pos = 0;
 	for (i = 0; i < strlen(cwd); ++i)
 	{
 		flag = 0;
@@ -33,7 +40,38 @@ int main(int argc, char const *argv[])
 			break;	
 		}
 	}
-	// printf("%d , %s\n",pos, username);
-	printf("<%s@%s:~%s>", username, hostname, cwd+pos);
+	shell_loop();
 	return 0;
+}
+
+void shell_loop(void)
+{
+	char *in_line;
+
+	while(1)
+	{
+		printf("<%s@%s:~%s>", username, hostname, cwd+pos);
+		in_line = readCommand();
+		printf("%s\n", in_line);
+	}
+}
+
+char* readCommand(void)
+{
+	char *buffer = malloc(sizeof(char) * 128);
+	int c = 0;
+	int pos = 0;
+	while(1)
+	{
+		c = getchar();
+		if(c == EOF || c == '\n')
+		{
+			buffer[pos] = '\0';
+			return buffer;
+		}
+		else
+		{
+			buffer[pos++] = c;
+		}
+	}
 }
