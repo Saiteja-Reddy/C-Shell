208a9471cfc0879ee85678e13b7a0fa933b36ade
Saiteja Reddy <saiteja0317@gmail.com>
2017-08-30 11:02:44 +0530

    Basic Child Forking - No error handling

--- Shell/main.c
+++ Shell/main.c
@@ -6,8 +6,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define TOKEN_BUFSIZE 64
+#define DELIMITERS " \t\r\n\a"
+
 void shell_loop(void);
 char* readCommand(void);
+int launchProcess(char **);
+char **splitCommand(char *);
 
 char hostname[256];
 char username[256];
@@ -30,11 +35,14 @@ int main(int argc, char const *argv[])
 void shell_loop(void)
 {
 	char *in_line;
-
+	char **args;
+	int out;
 	while(1)
 	{
 		printf("<%s@%s:~%s>", username, hostname, wd);
 		in_line = readCommand();
+		args = splitCommand(in_line);
+		out = launchProcess(args);
 		printf("%s\n", in_line);
 	}
 }
@@ -47,3 +55,45 @@ char* readCommand(void)
 	return buffer;
 }
 
+char **splitCommand(char *line)
+{
+	unsigned long bufsize = TOKEN_BUFSIZE;
+	int position = 0;
+	char **args = malloc(sizeof(char*) * bufsize);
+	char *arg;	
+	if(!args)
+	{
+		fprintf(stderr, "shell: Allocation Error\n");
+		exit(1);
+	}	
+
+	arg = strtok(line, DELIMITERS);
+	while(arg != NULL)
+	{
+		args[position++] = arg;
+		arg = strtok(NULL, DELIMITERS);
+	}
+	args[position] = NULL;
+	return args;
+}
+
+int launchProcess(char **args)
+{
+	pid_t pid;
+
+	pid = fork();
+	if(pid == 0)
+	{
+		execvp(args[0], args);
+	}
+	else if(pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		fprintf(stderr, "shell: Error Forking Child\n");
+		exit(1);		
+	}
+	return 1;
+}
