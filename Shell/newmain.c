ea11909d483ff98a73350ab8c4acf2ff8d8f79ff
Siddhartha Laghuvarapu <siddharthalsd@gmail.com>
2017-09-01 16:07:32 +0530

    Nightwatch dirty implemented

--- Shell/newmain.c
+++ Shell/newmain.c
@@ -22,6 +22,7 @@ char* idCommand(char *);
 int run_watch(char **);
 void nwInterrupt(int );
 void printWatch(char **, int*);
+void nwDirty(int);
 
 char hostname[256];
 char username[256];
@@ -317,6 +318,7 @@ void printWatch(char **args, int* args_len)
 	{
 		/// Dirty Code here
 		printf("Dirty Here\n");
+		nwDirty(time_int);
 	}
 	else
 	{
@@ -366,6 +368,58 @@ void nwInterrupt(int time_int)
 			getline(&buffer, &bufsize, fd);
 				getline(&buffer, &bufsize, fd);
 			getline(&buffer, &bufsize, fd);			
+			mvwaddstr(win, k, 10, buffer);
+			fclose(fd);
+
+		}
+		if(k>25)
+		{
+			wclear(win);
+			k=5;
+
+		}
+
+		wrefresh(win);
+	}
+		noecho();
+
+	endwin();
+
+}
+void nwDirty(int time_int)
+{
+	char c;
+	WINDOW* curr = initscr();
+	keypad(stdscr, TRUE);
+	WINDOW * win; 
+	win = newwin(800,600,1,1);
+	noecho();
+	curs_set(0);
+	nodelay(win,1);
+	int j = 10;
+	int k = 0;
+	int i;
+	int start = time(NULL) , current,prevcurrent = time(NULL);
+	k=5;
+	unsigned long bufsize = 0;
+	char *buffer;
+	char label[200];
+	FILE * fd;
+	sprintf(label, "\t%s\tTime specified = %d s","NIGHTSWATCH -- dirty",time_int);
+	while(1)
+	{
+		current = time(NULL);
+		if(wgetch(win) == 'q')
+			break;
+		mvwaddstr(win,1,10,label);
+		if((current- start)%time_int == 0 && current!=prevcurrent)
+		{ 
+			fd = fopen("/proc/meminfo", "r");
+			prevcurrent = current;
+			k += 1;
+			fseek(fd, 0, SEEK_SET);	
+			for(i=0;i<17;i++)		
+				getline(&buffer, &bufsize, fd);			
 			mvwaddstr(win, k, 10, buffer);
 			fclose(fd);
 
