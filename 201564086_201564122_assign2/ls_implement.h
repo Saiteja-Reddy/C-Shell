db1d4acb4a96b4afbc8337f3f65943e0f6a1341b
Saiteja-Reddy <saiteja0317@gmail.com>
2017-09-02 13:34:11 +0530

    Final Submission Added

---
+++ 201564086_201564122_assign2/ls_implement.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <grp.h>
+// #include <uuid/uuid.h>
+#include <time.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+
+int run_ls(char **);
+int printFileDir(char *, int []);
+char f_type(mode_t);
+char* getMonth(int);
+void PrintFileInfo(struct stat);
+long long getTotalBlocks(char*);
